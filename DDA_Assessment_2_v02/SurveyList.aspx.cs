ee5f1070d0838bce0d94b842f9fe429d7f64cd7c
Ivan Olea <ivolea@hotmail.com>
2017-09-14 17:17:22 +1000

    building table of survey's

---
+++ DDA_Assessment_2_v02/SurveyList.aspx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class SurveyList : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        //open connection
+        //get list of surveys and survey id's from db
+        //close connection
+        //populate place holder
+
+        //get selected
+
+        Button but1 = new Button();
+        Button but2 = new Button();
+        but1.Text = "button 1";
+        but2.Text = "button 2";
+        but1.Click += new EventHandler();
+        tableOfSurveys.Controls.Add(but1);
+        tableOfSurveys.Controls.Add(but2);
+
+    }
+
+    private void But1_Click(object sender, EventArgs e)
+    {
+        throw new NotImplementedException();
+    }
+}
