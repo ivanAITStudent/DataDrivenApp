f9823fd778de8295778ee025c5cb83eafca73f5e
Ivan Olea <ivolea@hotmail.com>
2017-09-21 06:00:17 +1000

    Building Uncle Bobs Classes

---
+++ DDA_Assessment_2_v03/Requester/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Requester
+{
+    /*
+     * The Requester initiates requests to the application for the View
+     * It does this through a Request Model
+     * This model varies depending on View's request
+     * Request Options For Surveys:
+     * Request Next Question
+     * Request Previous Question
+     * Request Current Question
+     * End Survey
+     * Begin Survey
+    */
+    public class Class1
+    {
+    }
+}
---
+++ DDA_Assessment_2_v03/SessionManager/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SessionManager
+{
+    /* The Session Manager manages the session
+     * it manages the state of the session
+     * it manages the requests and presentation models 
+     * it converses with application 
+     * it mainatins user session info (user id, username, user type, logged in or not, )
+     * 
+     */
+    public class Class1
+    {
+    }
+}
